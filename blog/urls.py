cdf0aea95608ee19c43d113eb2ac590363f89bf3
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2021-07-04 16:28:46 +0200

    customized blog pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts", views.posts, name="posts-page"),
-    path("posts/<str:blog_name>", views.blog_post, name="posts-detail-page")
+    path("posts/<slug:slug>", views.blog_post, name="posts-detail-page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,79 @@
-
+from datetime import date
 from django.http.response import Http404
 from django.shortcuts import render
 
 # Create your views here.
 
-blog_posts = {
-    "The First Blog": """There's nothing like the views you get when hiking in the mountains! 
-    And i wasn't even prepared for what happend whilist i was enjoying the view!""",
-    "The Second Blog": "This is my second blog, that i would like to share with you",
-    "The Third Blog": "This is my third blog, what i would like to share with you",
+blog_posts = [{
+    "slug": "the-first-blog",
+    "author": "David Ermakov",
+    "image": "mountains.jpg",
+    "post_title": "The-First-Blog",
+    "excerpt": """There's nothing like the views you get when hiking in the mountains! 
+        And i wasn't even prepared for what happend whilist i was enjoying the view!""",
+    "post_content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quaerat?
+        Possimus, quam saepe? Voluptatem, sunt. Officia possimus cumque
+        necessitatibus eos earum iste optio, asperiores sunt doloribus. Iste culpa
+        blanditiis porro.
+    """,
+
+    "date": date(2021, 7, 21),
+},
+    {
+        "author": "David Ermakov",
+        "image": "woods.jpg",
+        "slug": "the-secong-blog",
+        "post_title": "The Second Blog",
+        "excerpt": "This is my second blog, that i would like to share with you",
+        "post_content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quaerat?
+        Possimus, quam saepe? Voluptatem, sunt. Officia possimus cumque
+        necessitatibus eos earum iste optio, asperiores sunt doloribus. Iste culpa
+        blanditiis porro.
+    """,
+        "date": date(2021, 9, 25),
+},
+    {
+        "author": "David Ermakov",
+        "image": "coding.jpg",
+        "slug": "the-third-blog",
+        "post_title": "The Third Blog",
+        "excerpt": "This is my third blog, what i would like to share with you",
+        "post_content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quaerat?
+        Possimus, quam saepe? Voluptatem, sunt. Officia possimus cumque
+        necessitatibus eos earum iste optio, asperiores sunt doloribus. Iste culpa
+        blanditiis porro.
+    """,
+        "date": date(2022, 3, 11),
 }
 
+]
 
-def starting_page(reuqest):
-    pass
+
+def get_date(post):
+    return post["date"]
+
+
+def starting_page(request):
+    sorted_posts = sorted(blog_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {"blogs": latest_posts})
 
 
 def posts(request):
-    return render(request, "blog/index.html", {"blogs": blog_posts})
+    return render(request, "blog/all-posts.html", {
+        "all_posts": blog_posts
+    })
 
 
-def blog_post(request, blog_name):
+def blog_post(request, slug):
     try:
-        blog_content = blog_posts[blog_name]
+        identified_post = next(
+            post for post in blog_posts if post['slug'] == slug)
         return render(request, "blog/blog.html", {
-            "blog_name": blog_name,
-            "blog_content": blog_content
+            "post": identified_post
         })
     except:
         return Http404()
