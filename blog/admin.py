6ed312f43ce5d11d78f3bbbdb07a5005302bed57
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2021-07-13 17:07:00 +0200

    added data from db

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("last_name",)
+    list_display = ("full_name", "e_mail")
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "title", "tags")
+    list_display = ("title", "date", "author")
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
+
--- blog/views.py
+++ blog/views.py
@@ -1,55 +1,11 @@
-from datetime import date
 from django.http.response import Http404
 from django.shortcuts import render
+from .models import Post, Author, Tag
 
 # Create your views here.
 
-blog_posts = [{
-    "slug": "the-first-blog",
-    "author": "David Ermakov",
-    "image": "mountains.jpg",
-    "post_title": "The-First-Blog",
-    "excerpt": """There's nothing like the views you get when hiking in the mountains! 
-        And i wasn't even prepared for what happend whilist i was enjoying the view!""",
-    "post_content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quaerat?
-        Possimus, quam saepe? Voluptatem, sunt. Officia possimus cumque
-        necessitatibus eos earum iste optio, asperiores sunt doloribus. Iste culpa
-        blanditiis porro.
-    """,
 
-    "date": date(2021, 7, 21),
-},
-    {
-        "author": "David Ermakov",
-        "image": "woods.jpg",
-        "slug": "the-secong-blog",
-        "post_title": "The Second Blog",
-        "excerpt": "This is my second blog, that i would like to share with you",
-        "post_content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quaerat?
-        Possimus, quam saepe? Voluptatem, sunt. Officia possimus cumque
-        necessitatibus eos earum iste optio, asperiores sunt doloribus. Iste culpa
-        blanditiis porro.
-    """,
-        "date": date(2021, 9, 25),
-},
-    {
-        "author": "David Ermakov",
-        "image": "coding.jpg",
-        "slug": "the-third-blog",
-        "post_title": "The Third Blog",
-        "excerpt": "This is my third blog, what i would like to share with you",
-        "post_content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, quaerat?
-        Possimus, quam saepe? Voluptatem, sunt. Officia possimus cumque
-        necessitatibus eos earum iste optio, asperiores sunt doloribus. Iste culpa
-        blanditiis porro.
-    """,
-        "date": date(2022, 3, 11),
-}
-
-]
+blog_postics=Post.objects.all()
 
 
 def get_date(post):
@@ -57,21 +13,21 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(blog_posts, key=get_date)
+    sorted_posts = blog_postics.order_by("-date")
     latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {"blogs": latest_posts})
 
 
 def posts(request):
     return render(request, "blog/all-posts.html", {
-        "all_posts": blog_posts
+        "all_posts": blog_postics
     })
 
 
 def blog_post(request, slug):
     try:
         identified_post = next(
-            post for post in blog_posts if post['slug'] == slug)
+            post for post in blog_postics if post['slug'] == slug)
         return render(request, "blog/blog.html", {
             "post": identified_post
         })
